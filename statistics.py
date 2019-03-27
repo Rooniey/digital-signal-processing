fae75c64c0c943c2b29a464a53167bbd50812c9f
Rooniey - Marek Maras <210267@edu.p.lodz.pl>
2019-03-28 00:09:33 +0100

    Implemented calculating integral

---
+++ statistics.py
@@ -0,0 +1,24 @@
+from operations import computeSignal
+from utility import pluck
+import numpy as np
+
+n = 1000
+fp = 1000
+
+def computeIntegral(signal):
+    params = signal["params"]
+    t1 = params["t1"]
+    x = None
+    if signal["isPeriodic"]:
+        T =  params["T"]
+        x = np.linspace(t1, t1+T, n)
+    else:
+        d = params["d"]
+        x = np.linspace(t1, t1+d, int(fp*d))
+    
+    h = abs(x[1] - x[0]) 
+    y = computeSignal(signal, x)
+    data = sum(y[1:]) + sum(y[:-1])
+    return (h/2)*data
+
+
--- utility.py
+++ utility.py
@@ -1,1 +1,2 @@
 pluck = lambda d, *args: (d[arg] for arg in args)
+
